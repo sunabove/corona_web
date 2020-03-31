d90023d2881f6c5e15c1b727ace598a4d962b23e
sunabove <sunabove@users.noreply.github.com>
2020-04-01 07:52:40 +0900

    **

--- src/main/java/web/spring/CoronaMapApplication.java
+++ src/main/java/web/spring/CoronaMapApplication.java
@@ -1,7 +1,6 @@
 package web.spring;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
@@ -9,7 +8,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-//@EnableAutoConfiguration
 @Configuration
 @SpringBootApplication
 @ComponentScan( "web" )
