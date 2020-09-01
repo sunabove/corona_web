4fd4bb6a045a807db182f1241d0d670a672fa8a6
openvisual <sunabove@nate.com>
2020-09-01 17:05:52 +0900

    데이터 소스 설정을 CoronaMapApplication.java 로 이동함.

--- src/main/java/web/spring/CoronaMapApplication.java
+++ src/main/java/web/spring/CoronaMapApplication.java
@@ -3,10 +3,14 @@ package web.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import javax.sql.DataSource;
 
 @Configuration
 @SpringBootApplication
@@ -18,6 +22,30 @@ public class CoronaMapApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CoronaMapApplication.class, args);
+	}
+
+	@Bean
+	public DataSource getDataSource() {
+		/**
+		 * spring.datasource.url=jdbc:postgresql://127.0.0.1:5432/map
+		 * spring.datasource.username=postgres
+		 * spring.datasource.password=a
+		 * spring.datasource.driver-class-name=org.postgresql.Driver
+
+		 * #spring.jpa.database-platform = org.hibernate.dialect.PostgreSQLDialect
+		 * spring.jpa.database-platform=org.hibernate.spatial.dialect.postgis.PostgisDialect
+		 * spring.jpa.generate-ddl=true
+		 * spring.jpa.show-sql=true
+		 * spring.jpa.hibernate.ddl-auto = update
+		 */
+		DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+
+		dataSourceBuilder.url("jdbc:postgresql://127.0.0.1:5432/map");
+		dataSourceBuilder.username("postgres");
+		dataSourceBuilder.password("a");
+		dataSourceBuilder.driverClassName("org.postgresql.Driver");
+
+		return dataSourceBuilder.build();
 	}
 
 }
